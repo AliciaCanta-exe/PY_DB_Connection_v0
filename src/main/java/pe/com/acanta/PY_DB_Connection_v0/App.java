2a49dda3829dc0d31f490d8959dd21ef72f09e54
aliciacanta2016 <alicia.canta.exe@gmail.com>
2016-10-18 17:18:31 -0500

    primer commit de fuentes

---
+++ src/main/java/pe/com/acanta/PY_DB_Connection_v0/App.java
@@ -0,0 +1,13 @@
+package pe.com.acanta.PY_DB_Connection_v0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
